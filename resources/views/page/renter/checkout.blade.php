aecbda5185a314a78f06a70e2f8a4f1ad4e1b681
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2023-01-21 07:55:52 +0700

    add form in renter's checkout

--- resources/views/page/renter/checkout.blade.php
+++ resources/views/page/renter/checkout.blade.php
@@ -2,6 +2,7 @@
 
 @push('add-on-style')
   <link rel="stylesheet" href="/css/component/navbar.css" />
+  <link rel="stylesheet" href="/css/page/renter/checkout.css"
 @endpush
 
 @section('title')
@@ -11,7 +12,23 @@
 @section('content')
   @include('include.component.navbar_login_renter')
   <hr>
-  <h1>Checkout</h1>
+  <div id="formContainer">
+    <form id="formCheckout" class="d-flex d-flex-column">
+      <h1>Checkout</h1>
+      <div class="row">
+        <div class="left-side"><p>Nama</p></div>
+        <div class="right-side"><p>Mazda CX-9</p></div>
+      </div>
+      <div class="row">
+        <div class="left-side"><p>No Polisi</p></div>
+        <div class="right-side"><p>D 123 ABC</p></div>
+      </div>
+      <div class="row">
+        <div class="left-side"><p>Tipe</p></div>
+        <div class="right-side"><p>Kuro Edition</p></div>
+      </div>
+    </form>
+  </div>
 @endsection
 
 @push('add-on-script')
